ec6a78166206f5dcb757d2bdd2f73d2d900461a1
Alexander Holovin <dvjmixxxer@gmail.com>
2017-07-26 12:19:24 +0300

    Priority support, logging fix, error handling, update docs

--- README.md
+++ README.md
@@ -9,34 +9,12 @@ With `pip` (or `conda`):
 1. Run script `python3 main.py`
 
 ## Config .env description
-- `SITE_URL` - site url for parse 
+- `SITE_URL_{NUMBER}` - site url's for parse
 - `SITE_PAGE_PARAM` - page param for iterate
-- `APP_OUTPUT_CSV` - parse result file
+- `APP_OUTPUT_DIR` - output dir
+- `APP_OUTPUT_ENC` - output encoding
 - `APP_THREAD_COUNT` - count of threads for grub.spider
 - `APP_LOG_FORMAT` - log format (in python logger format)
 - `APP_LOG_FILE` - path to log file
 - `APP_WORK_MODE` - `dev` value sets DEBUG mode for all loggers, otherwise INFO 
-
-## Requirements
-- `Python 3.6.1`
-
-### Conda list
-`lxml=3.8.0=py36_0
-pip=9.0.1=py36_1
-pycurl=7.43.0=py36_2
-python=3.6.1=2
-setuptools=27.2.0=py36_1
-wheel=0.29.0=py36_0`
-
-### Pip freeze
-`click==6.7
-defusedxml==0.5.0
-grab==0.6.38
-lxml==3.8.0
-pycurl==7.43.0
-python-dotenv==0.6.4
-pytils==0.3
-selection==0.0.13
-six==1.10.0
-user-agent==0.1.8
-weblib==0.1.24`
+- `APP_TRY_LIMIT` - number of repeat tasks
--- d_spider.py
+++ d_spider.py
@@ -3,7 +3,6 @@ import re
 import urllib.parse
 
 from grab.spider import Spider, Task
-from weblib.error import DataNotFound
 
 from config.config import Config
 from helpers.url_generator import UrlGenerator
@@ -19,8 +18,8 @@ class DSpider(Spider):
     re_count = re.compile('^\d+$')
     re_price = re.compile('^\d+(.\d+)?$')
 
-    def __init__(self, thread_number, logger_name, writer):
-        super().__init__(thread_number)
+    def __init__(self, thread_number, logger_name, writer, try_limit=0):
+        super().__init__(thread_number=thread_number, network_try_limit=try_limit, priority_mode='const')
         self.logger = logging.getLogger(logger_name)
         self.result = writer
         self.logger.info('Init parser ok...')
@@ -33,34 +32,34 @@ class DSpider(Spider):
 
             if len(match.groups()) == 1:
                 page_number = match.group(1)
-                self.logger.debug('Find max_page: {}'.format(page_number))
+                self.logger.debug('[prep] Find max_page: {}'.format(page_number))
 
                 int_page_number = int(page_number)
 
                 if int_page_number > max_page:
-                    self.logger.debug('Set new max_page: {} => {}'.format(max_page, page_number))
+                    self.logger.debug('[prep] Set new max_page: {} => {}'.format(max_page, page_number))
                     max_page = int_page_number
 
         else:
-            self.logger.debug('Max page is: {}'.format(max_page))
+            self.logger.debug('[prep] Max page is: {}'.format(max_page))
 
             if max_page < 1:
-                err = 'Bad page counter: {}'.format(max_page)
+                err = '[prep] Bad page counter: {}'.format(max_page)
                 self.logger.error(err)
                 raise Exception(err)
 
-        self.logger.info('Task: {}, max_page: {}'.format(task.url, max_page))
+        self.logger.info('[prep] Task: {}, max_page: {}'.format(task.url, max_page))
 
         url_gen = UrlGenerator(task.url, Config.get('SITE_PAGE_PARAM'))
 
         for p in range(1, max_page + 1):
             url = url_gen.get_page(p)
-            yield Task('parse_page', url=url)
+            yield Task('parse_page', url=url, priority=90)
 
-        self.logger.info('Tasks added...')
+        self.logger.info('[prep] Tasks added...')
 
     def task_parse_page(self, grab, task):
-        self.logger.info('Find tasks: {}'.format(task.url))
+        self.logger.info('[page] Find tasks: {}'.format(task.url))
         rows = grab.doc.select('//div[@class="products"]/table[@class="prod"]//tr[not(contains(@class, "white"))]')
 
         for index, row in enumerate(rows):
@@ -68,8 +67,8 @@ class DSpider(Spider):
             count = row.select('./td[2]').text().strip()
             # skip useless tasks
             if count == '0':
-                self.logger.warning('Skip task list {} (line: {}), because count is {}'
-                                    .format(task.url, index, count))
+                self.logger.debug('[page] Skip at {} (line: {}), not in store ({})'
+                                  .format(task.url, index, count))
                 continue
 
             # PRICE
@@ -81,36 +80,31 @@ class DSpider(Spider):
 
             # skip useless tasks
             if price == 'под заказ':
-                self.logger.warning('Skip task list {} (line: {}), because price is {}'
-                                    .format(task.url, index, price))
+                self.logger.debug('[page] Skip at {} (line: {}), not available ({})'
+                                  .format(task.url, index, price))
                 continue
 
             # check regex
             if not self.re_price.match(price):
-                self.logger.warning('Skip task list {} (line: {}), because price is not float {}'
+                self.logger.warning('[page] Skip at {} (line: {}), not valid price format {}'
                                     .format(task.url, index, price))
                 continue
 
             # check less zero
             if float(price) <= 0:
-                self.logger.warning('Skip task list {} (line: {}), because price less zero {}'
+                self.logger.warning('[page] Skip at {} (line: {}), price invalid {}'
                                     .format(task.url, index, price))
                 continue
 
             # URL
             url = row.select('./td[@class="name"]/a').attr('href')
             url = urllib.parse.urljoin(self.domain, url)
-            self.logger.debug('Add page: {}'.format(url))
+            self.logger.debug('[page] Add page: {}'.format(url))
 
-            yield Task('parse_items', url=url)
-
-    def task_parse_page_fallback(self, task):
-        err = 'Url {} request failed! Try less APP_THREAD_COUNT!'.format(task.url)
-        self.logger.fatal(err)
-        print(err)
+            yield Task('parse_items', url=url, priority=100)
 
     def task_parse_items(self, grab, task):
-        self.logger.debug('Parse page: {}'.format(task.url))
+        self.logger.debug('[items] Parse page: {}'.format(task.url))
 
         try:
             rows = grab.doc.select('//div[@id="primary_block"]//table[@class="prod"]//tr[contains(@class, "product")]')
@@ -119,12 +113,12 @@ class DSpider(Spider):
                 count = row.select('./td[@class="st"]').text().strip()
                 # skip if count is number
                 if not self.re_count.match(count):
-                    self.logger.warning('[skip] Text {} is not a number (url: {})'
+                    self.logger.warning('[items] Text {} is not a number, skip (url: {})'
                                         .format(count, task.url))
                     continue
                 # skip if count less zero
                 if int(count) < 1:
-                    self.logger.warning('[skip] Number {} is less than zero (url: {})'
+                    self.logger.warning('[items] Number {} is less than zero, skip (url: {})'
                                         .format(count, task.url))
                     continue
 
@@ -135,13 +129,13 @@ class DSpider(Spider):
                     .strip()
                 # check regex
                 if not self.re_price.match(price):
-                    self.logger.warning('[skip] Price {} (line: {}), because price is not float {}'
-                                        .format(task.url, index, price))
+                    self.logger.warning('[items] Skip item, because price is {} (line: {})'
+                                        .format(price, index, ))
                     continue
 
                 # skip if zero
                 if float(price) <= 0:
-                    self.logger.warning('[skip] Price {} is less than zero (url: {})'
+                    self.logger.warning('[items] Price {} is less than zero, skip (url: {})'
                                         .format(price, task.url))
                     continue
 
@@ -162,8 +156,15 @@ class DSpider(Spider):
             err = 'Url {} parse failed (e: {})'.format(task.url, e)
             print(err)
             self.logger.error(err)
+            yield Task('parse_items', url=task.url, task_try_count=task.task_try_count + 1)
+
+    def task_parse_page_fallback(self, task):
+        self._go_err(task)
 
     def task_parse_items_fallback(self, task):
+        self._go_err(task)
+
+    def _go_err(self, task):
         err = 'Url {} request failed! Try less APP_THREAD_COUNT!'.format(task.url)
         self.logger.fatal(err)
         print(err)
