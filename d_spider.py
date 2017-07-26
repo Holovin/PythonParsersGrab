8b60763acd71735107fa6f0e116859cc8af37bb8
Alexander Holovin <dvjmixxxer@gmail.com>
2017-07-26 03:11:27 +0300

    Prerelease parser version for ekc

--- d_spider.py
+++ d_spider.py
@@ -1,5 +1,6 @@
 import logging
 import re
+import urllib.parse
 
 from grab.spider import Spider, Task
 from weblib.error import DataNotFound
@@ -12,18 +13,23 @@ from helpers.url_generator import UrlGenerator
 # noinspection PyUnusedLocal
 class DSpider(Spider):
     initial_urls = Config.get_seq('SITE_URL')
+    domain = '{uri.scheme}://{uri.netloc}/'.format(uri=urllib.parse.urlparse(Config.get_seq('SITE_URL')[0]))
+
+    re_page_number = re.compile('{}=(\d+)'.format(Config.get('SITE_PAGE_PARAM')))
+    re_count = re.compile('^\d+$')
+    re_price = re.compile('^\d+(.\d+)?$')
 
     def __init__(self, thread_number, logger_name, writer):
         super().__init__(thread_number)
         self.logger = logging.getLogger(logger_name)
         self.result = writer
+        self.logger.info('Init parser ok...')
 
     def task_initial(self, grab, task):
-        re_page_number = re.compile('{}=(\d+)'.format(Config.get('SITE_PAGE_PARAM')))
         max_page = 0
 
         for page_link in grab.doc.select('//a[contains(@href, "{}")]'.format(Config.get('SITE_PAGE_PARAM'))):
-            match = re_page_number.search(page_link.attr('href'))
+            match = self.re_page_number.search(page_link.attr('href'))
 
             if len(match.groups()) == 1:
                 page_number = match.group(1)
@@ -47,61 +53,108 @@ class DSpider(Spider):
 
         url_gen = UrlGenerator(task.url, Config.get('SITE_PAGE_PARAM'))
 
-        for p in range(1, 2): #max_page + 1):
+        for p in range(1, max_page + 1):
             url = url_gen.get_page(p)
-            print(url)
             yield Task('parse_page', url=url)
 
         self.logger.info('Tasks added...')
 
     def task_parse_page(self, grab, task):
-        self.logger.info('Start task: {}'.format(task.url))
-
-        rows = grab.doc.select('//div[@class="products"]/table[@class="prod"]//tr[not(contains(@class, "white"))]'
-                               '//td[@class="name"]/a')
+        self.logger.info('Find tasks: {}'.format(task.url))
+        rows = grab.doc.select('//div[@class="products"]/table[@class="prod"]//tr[not(contains(@class, "white"))]')
 
         for index, row in enumerate(rows):
-            try:
-                # TODO: get hosts
-                # TODO: add new tasks
-                print(row.attr('href'))
-                # item_name = row.select('./td[@class="item_name"]').text()
-                # pic = row.select('./td[@class="pic"]//img').attr('src')
-                # count = row.select('./td[@class="item_quant"]').text()
-                # inner_d = row.select('./td[@class="innerd"]').text()
-                # outer_d = row.select('./td[@class="outerd"]').text()
-                # width = row.select('./td[@class="width"]').text()
-                # price = row.select('./td[@class="price"]').text()
-
-            except DataNotFound:
-                self.logger.debug('Skip line {}'.format(index))
+            # COUNT
+            count = row.select('./td[2]').text().strip()
+            # skip useless tasks
+            if count == '0':
+                self.logger.warning('Skip task list {} (line: {}), because count is {}'
+                                    .format(task.url, index, count))
                 continue
 
-            # self.logger.debug('Index: {} | {} {} {} {} {} {}'
+            # PRICE
+            price = row.select('./td[@class="pr"]').text()\
+                .replace('от ', '', 1)\
+                .replace(' .', '', 1)\
+                .replace(',', '', 1).\
+                strip()
 
+            # skip useless tasks
+            if price == 'под заказ':
+                self.logger.warning('Skip task list {} (line: {}), because price is {}'
+                                    .format(task.url, index, price))
+                continue
 
-    # def task_parse_page(self, grab, task):
-    #     self.logger.info('Start task: {}'.format(task.url))
-    #
-    #     rows = grab.doc.select('//div[@class="content"]/table[@class="item_list"]//tr')
-    #     self.logger.debug('Index / Pic_Url / Name / Count / d / D / B / Price')
-    #
-    #     for index, row in enumerate(rows):
-    #         try:
-    #             item_name = row.select('./td[@class="item_name"]').text()
-    #             pic = row.select('./td[@class="pic"]//img').attr('src')
-    #             count = row.select('./td[@class="item_quant"]').text()
-    #             inner_d = row.select('./td[@class="innerd"]').text()
-    #             outer_d = row.select('./td[@class="outerd"]').text()
-    #             width = row.select('./td[@class="width"]').text()
-    #             price = row.select('./td[@class="price"]').text()
-    #
-    #         except DataNotFound:
-    #             self.logger.debug('Skip line {}'.format(index))
-    #             continue
-    #
-    #         self.result.writerow([item_name, count, price])
-    #         self.logger.debug('Index: {} | {} {} {} {} {} {}'
-    #                           .format(index, item_name, pic, count, inner_d, outer_d, width, price))
-    #
-    #     self.logger.info('Task {} end'.format(task.url))
+            # check regex
+            if not self.re_price.match(price):
+                self.logger.warning('Skip task list {} (line: {}), because price is not float {}'
+                                    .format(task.url, index, price))
+                continue
+
+            # check less zero
+            if float(price) <= 0:
+                self.logger.warning('Skip task list {} (line: {}), because price less zero {}'
+                                    .format(task.url, index, price))
+                continue
+
+            # URL
+            url = row.select('./td[@class="name"]/a').attr('href')
+            url = urllib.parse.urljoin(self.domain, url)
+            self.logger.debug('Add page: {}'.format(url))
+
+            yield Task('parse_items', url=url)
+
+    def task_parse_items(self, grab, task):
+        self.logger.debug('Parse page: {}'.format(task.url))
+
+        try:
+            rows = grab.doc.select('//div[@id="primary_block"]//table[@class="prod"]//tr[contains(@class, "product")]')
+            for index, row in enumerate(rows):
+                # COUNT
+                count = row.select('./td[@class="st"]').text().strip()
+                # skip if count is number
+                if not self.re_count.match(count):
+                    self.logger.warning('[skip] Text {} is not a number (url: {})'
+                                        .format(count, task.url))
+                    continue
+                # skip if count less zero
+                if int(count) < 1:
+                    self.logger.warning('[skip] Number {} is less than zero (url: {})'
+                                        .format(count, task.url))
+                    continue
+
+                # PRICE
+                price = row.select('./td[@class="pr"]').text()\
+                    .replace(' .', '', 1)\
+                    .replace(',', '', 1)\
+                    .strip()
+                # check regex
+                if not self.re_price.match(price):
+                    self.logger.warning('[skip] Price {} (line: {}), because price is not float {}'
+                                        .format(task.url, index, price))
+                    continue
+
+                # skip if zero
+                if float(price) <= 0:
+                    self.logger.warning('[skip] Price {} is less than zero (url: {})'
+                                        .format(price, task.url))
+                    continue
+
+                # UNIT
+                unit = row.select('./td[@class="but"]/form[@class="variants"]//tr/td[2]').text().strip()
+                # use default value
+                if unit == '':
+                    unit = 'ед.'
+
+                # NAME
+                item_name = row.select('./td[@class="name"]').text().strip()
+
+                # OUTPUT
+                output_line = '{} {} {} {}'.format(item_name, count, unit, price)
+                self.result.writerow([item_name, count, unit, price])
+
+        except Exception as e:
+            err = 'err: {}, url: {}'.format(e, task.url)
+            print(err)
+            self.logger.error(err)
+            exit(100)
