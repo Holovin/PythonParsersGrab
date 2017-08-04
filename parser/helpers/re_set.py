ff9d01d73274dfcf744cb6a2e5b62dd2df4b271a
Alexander Holovin <dvjmixxxer@gmail.com>
2017-08-04 17:53:55 +0300

    Update regex set

--- parser/helpers/re_set.py
+++ parser/helpers/re_set.py
@@ -4,6 +4,15 @@ from helpers.config import Config
 
 
 class Ree:
+    # {PARAM}=1, like p=1
     page_number = re.compile('{}=(?P<page>\d+)'.format(Config.get('SITE_PAGE_PARAM')))
+
+    # price.price, like [10.10]
+    # WARN: dot without backslash, possible errors, but its already used
     price = re.compile('^\d+(.\d+)?$')
+
+    # price{SEP}price, like 10.10 or 11,11
+    price_extractor = re.compile('(?P<price>\d+({}\d+)?)'.format(Config.get('APP_PRICE_SEP')))
+
+    # Just number check (maybe better check with python methods?)
     number = re.compile('^\d+$')
