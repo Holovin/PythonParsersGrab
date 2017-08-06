93dddeddcbc1b4f80ca31087971d6a157e2078cf
Alexander Holovin <dvjmixxxer@gmail.com>
2017-08-06 03:47:58 +0300

    Stub for get_body

--- d_parser/helpers/get_body.py
+++ d_parser/helpers/get_body.py
@@ -1,3 +1,3 @@
 # TODO: remove useless params
 def get_body(grab, encoding='cp1251', bom=False, skip_errors=True, fix_spec_chars=True):
-    return grab.doc.body.decode('utf-8', 'ignore')
+    return grab.doc.body
