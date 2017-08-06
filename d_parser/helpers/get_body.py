0e7fdc409c17870ada40f43f72b9b20b7f490519
Alexander Holovin <dvjmixxxer@gmail.com>
2017-08-06 03:16:48 +0300

    Rework get body text method

--- d_parser/helpers/get_body.py
+++ d_parser/helpers/get_body.py
@@ -1,2 +1,3 @@
+# TODO: remove useless params
 def get_body(grab, encoding='cp1251', bom=False, skip_errors=True, fix_spec_chars=True):
-    return grab.doc.convert_body_to_unicode(grab.doc.body, bom, encoding, skip_errors, fix_spec_chars)
+    return grab.doc.body.decode('utf-8', 'ignore')
