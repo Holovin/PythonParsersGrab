3f32e93e21db03a9254528c90fb14c2bb91a1ed9
Alexander Holovin <dvjmixxxer@gmail.com>
2017-08-07 17:03:27 +0300

    Err handling aca

--- d_parser/d_spider_aca.py
+++ d_parser/d_spider_aca.py
@@ -1,17 +1,14 @@
 import logging
-import urllib.parse
 
 from grab.spider import Spider, Task
 
 from d_parser.helpers import url_lib
-from d_parser.helpers.el_parser import get_max_page
-from helpers.config import Config
-from helpers.output import Output
-from helpers.url_generator import UrlGenerator
-from d_parser.extend.check_body_errors import check_body_errors
 from d_parser.helpers.cookies_init import cookies_init
-from d_parser.helpers.get_body import get_body
+from d_parser.helpers.el_parser import get_max_page
+from d_parser.helpers.parser_extender import check_body_errors, process_error
 from d_parser.helpers.re_set import Ree
+from helpers.config import Config
+from helpers.url_generator import UrlGenerator
 
 
 # Warn: Don't remove task argument even if not use it (it's break grab and spider crashed)
@@ -21,6 +18,10 @@ class DSpider(Spider):
 
     def __init__(self, thread_number, logger_name, writer, try_limit=0):
         DSpider._check_body_errors = check_body_errors
+        DSpider._process_error = process_error
+
+        Ree.is_number()
+        Ree.is_page_number(Config.get('SITE_PAGE_PARAM'))
 
         super().__init__(thread_number=thread_number, network_try_limit=try_limit, priority_mode='const')
 
@@ -37,23 +38,25 @@ class DSpider(Spider):
         return cookies_init(self.cookie_jar, g)
 
     def task_initial(self, grab, task):
-        max_page = 0
-
-        if self._check_body_errors(task, grab.doc, '[start]'):
+        if self._check_body_errors(grab, task, '[start]'):
             err = '[start] Err task with url {}, attempt {}'.format(task.url, task.task_try_count)
             self.logger.fatal(err)
             return
 
-        items = grab.doc.select('//div[@class="catalog-pagenav"]//a[contains(@href, "{}")]'.format(Config.get('SITE_PAGE_PARAM')))
-        max_page = get_max_page(items)
+        try:
+            items = grab.doc.select('//div[@class="catalog-pagenav"]//a[contains(@href, "{}")]'.format(Config.get('SITE_PAGE_PARAM')))
+            max_page = get_max_page(items)
 
-        self.logger.info('[prep] Task: {}, max_page: {}'.format(task.url, max_page))
+            self.logger.info('[prep] Task: {}, max_page: {}'.format(task.url, max_page))
 
-        url_gen = UrlGenerator(task.url, Config.get('SITE_PAGE_PARAM'))
+            url_gen = UrlGenerator(task.url, Config.get('SITE_PAGE_PARAM'))
 
-        for p in range(1, max_page + 1):
-            url = url_gen.get_page(p)
-            yield Task('parse_page', url=url, priority=90)
+            for p in range(1, max_page+1):
+                url = url_gen.get_page(p)
+                yield Task('parse_page', url=url, priority=90)
+
+        except Exception as e:
+            self._process_error(grab, task, e)
 
         self.logger.info('[prep] Tasks added...')
 
@@ -61,10 +64,10 @@ class DSpider(Spider):
         self.logger.debug('[items] Parse page: {}'.format(task.url))
 
         try:
-            if self._check_body_errors(task, grab.doc, '[items]'):
+            if self._check_body_errors(grab, task, '[items]'):
                 if task.task_try_count < self.err_limit:
                     self.logger.error('[items] Restart task with url {}, attempt {}'.format(task.url, task.task_try_count))
-                    yield Task('parse_items', url=task.url, priority=110, task_try_count=task.task_try_count + 1, raw=True)
+                    yield Task('parse_items', url=task.url, priority=110, task_try_count=task.task_try_count+1, raw=True)
                 else:
                     self.logger.error('[items] Skip task with url {}, attempt {}'.format(task.url, task.task_try_count))
 
@@ -88,15 +91,6 @@ class DSpider(Spider):
 
                 # PRICE
                 price = Config.get('APP_PRICE_ON_REQUEST')
-                # price = row.select('.//td[3]/b/span').text().strip()
-                # # check regex
-                # price_re_result = Ree.price.match(price)
-                # if (not price_re_result or (price_re_result and float(price) < 0)) and price != 'по запросу':
-                #     self.logger.warning('[items] Skip item, because price is {} (line: {})'
-                #                         .format(price, index, ))
-                #     continue
-                # # replace
-                # if price == 'по запросу':
 
                 # NAME
                 item_name = row.select('./div[@class="name"]').text().strip()
@@ -110,10 +104,7 @@ class DSpider(Spider):
                 self.result.writerow([item_name, count, unit, price])
 
         except Exception as e:
-            html = get_body(grab)
-            err = '[items] Url {} parse failed (e: {}), debug: {}'.format(task.url, e, html)
-            Output.print(err)
-            self.logger.error(err)
+            self._process_error(grab, task, e)
 
     # def task_parse_items(self, grab, task):
     #     if 'Узнать цену' not in get_body(grab):
