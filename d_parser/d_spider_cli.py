52d262674c729b92f345d75d981d7eb6d3ead18f
Alexander Holovin <dvjmixxxer@gmail.com>
2017-08-07 18:16:19 +0300

    CLI updated to v2.3

--- d_parser/d_spider_cli.py
+++ d_parser/d_spider_cli.py
@@ -2,13 +2,10 @@ import logging
 
 from grab.spider import Spider, Task
 
-from d_parser.helpers.url_lib import get_host_from_url
-from helpers.config import Config
-from helpers.output import Output
-from d_parser.extend.check_body_errors import check_body_errors
 from d_parser.helpers.cookies_init import cookies_init
-from d_parser.helpers.get_body import get_body
+from d_parser.helpers.parser_extender import check_body_errors, process_error, common_init
 from d_parser.helpers.re_set import Ree
+from helpers.config import Config
 
 
 # Warn: Don't remove task argument even if not use it (it's break grab and spider crashed)
@@ -16,34 +13,31 @@ from d_parser.helpers.re_set import Ree
 class DSpider(Spider):
     initial_urls = Config.get_seq('SITE_URL')
 
-    def __init__(self, thread_number, logger_name, writer, try_limit=0):
-        DSpider._check_body_errors = check_body_errors
-
+    def __init__(self, thread_number, writer, try_limit=0):
         super().__init__(thread_number=thread_number, network_try_limit=try_limit, priority_mode='const')
+        DSpider._check_body_errors = check_body_errors
+        DSpider._process_error = process_error
+        DSpider._common_init = common_init
+        self._common_init(writer, try_limit)
 
-        self.logger = logging.getLogger(logger_name)
-        self.result = writer
-        self.status_counter = {}
-        self.cookie_jar = {}
-        self.err_limit = try_limit
-        self.domain = get_host_from_url(Config.get_seq('SITE_URL')[0])
-        self.logger.info('Init parser ok...')
+        Ree.is_number()
+        Ree.is_page_number(Config.get('SITE_PAGE_PARAM'))
+        Ree.is_float()
 
     def create_grab_instance(self, **kwargs):
         g = super(DSpider, self).create_grab_instance(**kwargs)
         return cookies_init(self.cookie_jar, g)
 
     def task_initial(self, grab, task):
-        self.logger.debug('[items] Parse page: {}'.format(task.url))
+        self.logger.debug('[{}] Parse page: {}'.format(task.name, task.url))
 
         try:
-            if self._check_body_errors(task, grab.doc, '[items]'):
+            if self._check_body_errors(grab, task):
                 if task.task_try_count < self.err_limit:
-                    self.logger.error('[items] Restart task with url {}, attempt {}'.format(task.url, task.task_try_count))
+                    self.logger.error('[{}] Restart task with url {}, attempt {}'.format(task.name, task.url, task.task_try_count))
                     yield Task('parse_items', url=task.url, priority=110, task_try_count=task.task_try_count+1, raw=True)
                 else:
-                    self.logger.error('[items] Skip task with url {}, attempt {}'.format(task.url, task.task_try_count))
-
+                    self.logger.error('[{}] Skip task with url {}, attempt {}'.format(task.name, task.url, task.task_try_count))
                 return
 
             table_wrapper = grab.doc.select('//div[contains(@class, "table-wrapper")]')
@@ -58,17 +52,17 @@ class DSpider(Spider):
                 # replace all spaces, because count can be like '1 111' when its = 1111
                 count = row.select('./td[5]').text().replace(' ', '')
 
-                # skip if count is wrong [need here?]
+                # skip if count is wrong
                 if not Ree.number.match(count):
-                    self.logger.warning('[items] Text {} is not a number, skip (url: {})'.format(count, task.url))
+                    self.logger.warning('[{}] Text {} is not a number, skip (url: {})'.format(task.name, count, task.url))
                     continue
 
                 # PRICE
                 price_raw = row.select('./td[6]').text().strip()
-                match = Ree.price_extractor.match(price_raw)
+                match = Ree.float.match(price_raw)
                 # check & fix
                 if not match:
-                    self.logger.warning('[items] Skip item, because price is {} (line: {})'.format(price_raw, index))
+                    self.logger.warning('[{}] Skip item, because price is {} (line: {})'.format(task.name, price_raw, index))
                     continue
 
                 price = match.groupdict()['price'].replace(',', '.')
@@ -76,12 +70,13 @@ class DSpider(Spider):
                 # NAME
                 item_name = row.select('./td[2]').text().strip()
 
+                if not item_name:
+                    self.logger.debug('[{}] Text {} is not a name, skip (url: {})'.format(task.name, item_name, task.url))
+                    continue
+
                 # OUTPUT
-                self.logger.debug('[items] Item added, index {} at url {}'.format(index, task.url))
+                self.logger.debug('[{}] Item added, index {} at url {}'.format(task.name, index, task.url))
                 self.result.writerow([item_name, count, unit, price])
-                return
 
         except Exception as e:
-            err = '[items] Url {} parse failed (e: {}), debug: {}'.format(task.url, e, get_body(grab))
-            Output.print(err)
-            self.logger.error(err)
+            self._process_error(grab, task, e)
