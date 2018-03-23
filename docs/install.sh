2d724be232bef17c439cf601bdf58e550821b4e9
Alexander Holovin <dvjmixxxer@gmail.com>
2018-03-23 11:44:13 +0300

    Fix docs

--- docs/install.sh
+++ docs/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd ~
-wget -O python.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+wget -O python.sh https://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh
 bash python.sh -b
 rm python.sh
 
-export PATH=~/miniconda3/bin:$PATH
+export PATH=~/anaconda3/bin:$PATH
 conda create --name 'd_parsers' python=3.6 -y
 source activate d_parsers
 
