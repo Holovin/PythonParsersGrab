c3af3013178416ead4d5ed113bc1b96fc550228b
Alexander Holovin <dvjmixxxer@gmail.com>
2018-03-25 19:35:30 +0300

    Back to miniconda

--- docs/run.sh
+++ docs/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'You need specify site config name (example: python main.py test.env)'
-export PATH=~/anaconda3/bin:$PATH
+export PATH=~/miniconda3/bin:$PATH
 source activate d_parsers
 python ../main.py
 source deactivate
